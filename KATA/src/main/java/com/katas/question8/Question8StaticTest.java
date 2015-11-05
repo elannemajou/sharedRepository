4865d37ae8cce08716f1c4178e09cc54e1a0282d
unknown <elannemajou@gmail.com>
2015-11-05 18:36:55 +0100

    Time tested for Consummer Producer

--- KATA/src/main/java/com/katas/question8/Question8StaticTest.java
+++ KATA/src/main/java/com/katas/question8/Question8StaticTest.java
@@ -1,5 +1,6 @@
 package com.katas.question8;
 
+import java.util.Vector;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -10,8 +11,29 @@ public class Question8StaticTest {
 	private final static int QUEUE_SIZE = 100;
 	private final static int NB_ELEMENTS_TO_PRODUCE = 50;
 	
+	
 	public static void main(String[] args) {
-		 ExecutorService executorService = Executors.newFixedThreadPool(2);
+		timeAndComputeBeforeJava7();
+		timeAndComputeWithJava7();
+	}
+	
+	public static void timeAndComputeBeforeJava7(){
+		final long start = System.nanoTime();
+		computeBeforeJava7();
+		final long end = System.nanoTime();
+		System.out.println("Time (second) before java 7 taken is " + (end-start)/1.0e9);
+	}
+	
+	public static void timeAndComputeWithJava7(){
+		final long start = System.nanoTime();
+		computeWithJava7();
+		final long end = System.nanoTime();
+		System.out.println("Time (second) with java 7 taken is " + (end-start)/1.0e9);
+	}
+	
+	
+	public static void computeWithJava7(){
+		 ExecutorService executorService = Executors.newFixedThreadPool(8);
 		 BlockingQueue<Integer> sharedQueue = new ArrayBlockingQueue(QUEUE_SIZE);
 		 
 		 Thread prodThread = new Thread(new ProducerWithJava7(sharedQueue, NB_ELEMENTS_TO_PRODUCE), "Producer");
@@ -19,7 +41,15 @@ public class Question8StaticTest {
 	     executorService.execute(prodThread);
 	     executorService.execute(consThread);
 	     executorService.shutdown();
-
+	}
+	
+	public static void computeBeforeJava7(){
+ 		 Vector sharedQueue = new Vector();
+		 
+		 Thread prodThread = new Thread(new ProducerBeforeJava7(sharedQueue, QUEUE_SIZE, NB_ELEMENTS_TO_PRODUCE), "Producer");
+	     Thread consThread = new Thread(new ConsumerBeforeJava7(sharedQueue), "Consumer");
+	     prodThread.start();
+	     consThread.start();
 	}
 
 }
