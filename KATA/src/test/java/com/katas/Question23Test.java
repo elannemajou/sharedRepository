c5b8c6276e55213448c02f39de2d94c25682a08c
unknown <elannemajou@gmail.com>
2015-11-06 08:51:49 +0100

    Added exemple for Question23

---
+++ KATA/src/test/java/com/katas/Question23Test.java
@@ -0,0 +1,32 @@
+package com.katas;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import org.junit.Test;
+
+public class Question23Test {
+
+	@Test
+	public void testWithGoodHashCode() {
+		HashMap<String, String> stringMap = new HashMap<String, String>(); 
+		stringMap.put("Test1Key", "test1value");
+		assertEquals("test1Value", (String) stringMap.get("Test1Key"));
+	}
+	
+	@Test
+	public void testWithChangedHashCode() {
+		HashMap<Question23, String> stringMap = new HashMap<Question23, String>(); 
+		
+		Question23 q1 = new Question23();
+		q1.setInternalValue("Test1Value");
+		stringMap.put(q1, "test1value");
+		
+		Question23 q2 = new Question23();
+		q2.setInternalValue("Test1Value");
+		stringMap.put(q2, "test2value");
+		
+		assertEquals("test1Value", (String) stringMap.get(q1));
+	}
+
+}
